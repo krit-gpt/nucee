eac1db1a19c989955f7eb0ef4cef7c221efa4e37
krittt <krit.nsit@gmail.com>
2018-04-23 16:43:57 -0500

    mod

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php include_once("home.html"); ?>
+
+
+
+
+
+<?php
+ if(isset($_POST['submit2']))
+ {
+ 	console.log("IN the php file");
+   $output=shell_exec('sh /somePATH/cgi-bin/script.sh');
+   echo $output;
+ }
+?>
